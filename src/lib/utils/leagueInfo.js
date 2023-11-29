100666d10f79f68354a6fd98a688915610a20eaa
bschleter <42521730+bschleter@users.noreply.github.com>
2023-11-29 02:40:31 -0500

    Update leagueInfo.js
    fixed syntax errors in const managers function

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,79 +32,75 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
-     {
-       "roster": 1, 
-       "managerID: "736059696083021824",// ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "Terry3outof6524outof7",
-      //"tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "In your girl's DMs!", // (optional)
-       "bio": "Schleter talks like outer space. You know it’s out there, but it doesn’t make any sense.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nos", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
-        link: 6, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
-      "favoritePlayer": 4807, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "FavoritePosition": "FA", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "No Half Stepping 24/7 3/65",
-      "tradingScale": 10, // 1 - 10
-      "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-      }
-    {
-      "roster": 2, // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID: "734561861273567232",
-      "name": "Mixon It Up",
-      "location": "up Bob's ass", // (optional)
-      "bio": "Meet Jeremy: The League's Unofficial Referee"
-      "If our fantasy football league had a mascot, it would be Jeremy - mainly because we can't afford a real mascot, and Jeremy's always here, arguing about something. Known for his unique ability to turn a casual chat about football into a passionate debate on the aerodynamics of a pigskin, Jeremy is the heartbeat of our league's banter.
-      "As a brother-in-law to Bob (which we suspect is the universe's idea of a practical joke), Jeremy brings a special flavor to the group. He's the kind of guy who insists his fantasy team is based on complex algorithms, but we all know he picks players based on their hairstyles.
-      "On game day, you'll find Jeremy shouting at the TV like he's the one making the calls - and let's be honest, if shouting were a fantasy point, he'd win the league hands down. Off the field, he's an amateur...
-      "In short, Jeremy is our league's spark plug - always firing, occasionally misfiring, but definitely indispensable.", 
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "tbb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
-        link: 3, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/rival.jpg" , // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
-      "favoritePlayer": 1741, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy",
-      "tradingScale": 10, // 1 - 10
-      "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-      }
-    {
-      "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID: "734580107951374336", //
-      "name": "ProtoBob", //
-      "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "Global Warming Hell", // (optional)
-      "bio": "Because, in our league drafting  is like being in a drafty room. It's cold, slightly uncomfortable, and Bob's usually asleep somewhere in the corner.",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "tbb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-      "rival": {
-        name: "Rival", // Can be anything (usually your rival's name)
-        link: 2, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-      },
-      "favoritePlayer": 8557, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-      "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy",
-      "tradingScale": 10, // 1 - 10
-      "preferredContact": "Discord", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-      }
-  ]
+  {
+    "roster": 1, 
+    "managerID": "736059696083021824", // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    "name": "Terry3outof6524outof7",
+    // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    "location": "In your girl's DMs!", // (optional)
+    "bio": "Schleter talks like outer space. You know it’s out there, but it doesn’t make any sense.",
+    "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
+    "favoriteTeam": "nos", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild'
+    "rival": {
+      "name": "Rival", // Can be anything (usually your rival's name)
+      "link": 6, // manager array number within this array, or null to link back to all managers page
+      "image": "/managers/rival.jpg" // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    },
+    "favoritePlayer": 4807, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    "FavoritePosition": "FA", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets'
+    "philosophy": "No Half Stepping 24/7 3/65",
+    "tradingScale": 10, // 1 - 10
+    "preferredContact": "Text" // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon'
+  },
+  {
+    "roster": 2,
+    "managerID": "734561861273567232",
+    "name": "Mixon It Up",
+    "location": "up Bob's ass", // (optional)
+    "bio": "Meet Jeremy: The League's Unofficial Referee. If our fantasy football league had a mascot, it would be Jeremy - mainly because we can't afford a real mascot, and Jeremy's always here, arguing about something. Known for his unique ability to turn a casual chat about football into a passionate debate on the aerodynamics of a pigskin, Jeremy is the heartbeat of our league's banter. As a brother-in-law to Bob (which we suspect is the universe's idea of a practical joke), Jeremy brings a special flavor to the group. He's the kind of guy who insists his fantasy team is based on complex algorithms, but we all know he picks players based on their hairstyles. On game day, you'll find Jeremy shouting at the TV like he's the one making the calls - and let's be honest, if shouting were a fantasy point, he'd win the league hands down. Off the field, he's an amateur... In short, Jeremy is our league's spark plug - always firing, occasionally misfiring, but definitely indispensable.", 
+    "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
+    "favoriteTeam": "tbb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild'
+    "rival": {
+      "name": "Rival", // Can be anything (usually your rival's name)
+      "link": 3, // manager array number within this array, or null to link back to all managers page
+      "image": "/managers/rival.jpg" // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    },
+    "favoritePlayer": 1741, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets'
+    "philosophy": "Your fantasy team's philosophy",
+    "tradingScale": 10, // 1 - 10
+    "preferredContact": "Text" // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon'
+  },
+  {
+    "roster": 3,
+    "managerID": "734580107951374336",
+    "name": "ProtoBob",
+    "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    "location": "Global Warming Hell", // (optional)
+    "bio": "Because, in our league drafting is like being in a drafty room. It's cold, slightly uncomfortable, and Bob's usually asleep somewhere in the corner.",
+    "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+    "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
+    "favoriteTeam": "tbb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild'
+    "rival": {
+      "name": "Rival", // Can be anything (usually your rival's name)
+      "link": 2, // manager array number within this array, or null to link back to all managers page
+      "image": "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+    },
+    "favoritePlayer": 8557, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+    "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets'
+    "philosophy": "Your fantasy team's philosophy",
+    "tradingScale": 10, // 1 - 10
+    "preferredContact": "Discord" // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon'
+  }
+];
   
   
   /*   !!  !!  IMPORTANT  !!  !! */
